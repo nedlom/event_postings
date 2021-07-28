ed02ce0a18a3ba1404d6d675bb26d69a8f81e1a1
Nick <nelom3@gmail.com>
2021-07-28 06:51:28 -0700

    add has many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
     has_secure_password #access through bcrypt - can use AR method 'authenticate'. Checks string entered agains bcrypt's hashing algorithm.
+    has_many :events
 end

84d9c44311d2c21c9c5f0545ea6c11c88cfb4bed
Nick <nelom3@gmail.com>
2021-07-29 11:03:45 -0700

    adds index action to controller and index view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,12 @@
 class EventsController < ApplicationController
     # get events/new
 
+    get '/events' do 
+        @events = Event.all
+        erb :'/events/index'
+    end
+
+
     get '/events/new' do 
         erb :'/events/new'
     end

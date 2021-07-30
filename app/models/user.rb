c52fbb911f3279db668bfcbba23a2b708a4d6813
Nick <nelom3@gmail.com>
2021-07-29 18:48:20 -0700

    adds validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password #access through bcrypt - can use AR method 'authenticate'. Checks string entered agains bcrypt's hashing algorithm.
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniquneness: true
+    
     has_many :events
 end

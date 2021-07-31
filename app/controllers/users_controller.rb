f1b2c9a814c6f1c4c2ccc4453e3ea15a85f69516
Nick <nelom3@gmail.com>
2021-07-31 13:08:44 -0700

    adds redirect helper methods to controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     erb :'/users/signup'
   end
 
-  post '/users' do 
+  post '/signup' do 
     @user = User.new(params)
     if @user.save
       session[:user_id] = @user.id
@@ -39,23 +39,22 @@ class UsersController < ApplicationController
     end
   end
 
-  get '/users/:id' do 
-    if !logged_in?
-      redirect "/"
-    end
-
-    @user = User.find_by(id: params[:id])
-    if @user == current_user
-      erb :'users/show'
-    else 
-      flash[:errors] = "You don't have access to that page."
-      redirect "/events"
-    end
-  end
-
   get '/logout' do
     session.clear
     redirect '/'
   end
 
+  # get '/users/:id' do 
+  #   if !logged_in?
+  #     redirect "/"
+  #   end
+
+  #   @user = User.find_by(id: params[:id])
+  #   if @user == current_user
+  #     erb :'users/show'
+  #   else 
+  #     flash[:errors] = "You don't have access to that page."
+  #     redirect "/events"
+  #   end
+  # end
 end

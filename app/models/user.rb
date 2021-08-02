7a0f48f9144428ab93e27daf68b0346c1a603a75
Nick <nelom3@gmail.com>
2021-08-02 15:04:40 -0700

    remove comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_secure_password #access through bcrypt - can use AR method 'authenticate'. Checks string entered agains bcrypt's hashing algorithm.
+    has_secure_password 
     validates :name, presence: true
     validates :email, presence: true
     validates :email, uniqueness: true
--- db/migrate/20210728125416_create_users_table.rb
+++ db/migrate/20210728125416_create_users_table.rb
@@ -3,7 +3,7 @@ class CreateUsersTable < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password_digest #bcrypt keeps encrypted passwords in this column
+      t.string :password_digest 
       t.timestamps null: false
     end
   end

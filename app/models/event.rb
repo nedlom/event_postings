f74aca4ee66b42fe00c3c45877fd51ac37cddcdf
Nick <nelom3@gmail.com>
2021-08-04 08:46:34 -0700

    fix spacing

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ActiveRecord::Base
-    belongs_to :user
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :location, presence: true
-    validates :date, presence: true
-    validates :time, presence: true
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
-    has_secure_password 
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :email, uniqueness: true
-    
-    has_many :events
+  has_many :events
+  has_secure_password 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
